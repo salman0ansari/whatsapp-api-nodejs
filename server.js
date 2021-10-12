5508d1cf670b33c4dbcac5f338abeef99a74125f
Mohd Salman Ansari <salman0ansariii@gmail.com>
2021-10-12 10:35:34 +0530

    change: logger to morgan

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express()
-const fileupload = require("express-fileupload")
-const Logger = require("./Middleware/logger")
+// const fileupload = require("express-fileupload")
+const cors = require("cors");
+const morgan = require("morgan");
+const fs = require('fs')
+const swaggerUI = require("swagger-ui-express");
+const docs = require('./Docs');
 
-app.use(Logger)
-app.use(fileupload())
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(docs));
+// app.use(fileupload())
 app.set('view engine', 'ejs');
 app.set('views','./Views')
 app.use(express.static('./Views'));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.json());
+app.use(morgan('dev',{
+    stream: fs.createWriteStream('Logs/access.log', {flags: 'a'})
+}));
 app.use(express.Router())
 
 const Instance = require("./Routes/Instance")
@@ -19,6 +27,7 @@ const sendMessage = require("./Routes/sendMessage")
 app.use(Instance);
 app.use(sendMessage);
 
+
 app.get('*',(req, res) => {
     res.status(404).send({
         error: true,
@@ -26,6 +35,8 @@ app.get('*',(req, res) => {
     });
 })
 
-const server = app.listen(process.env.PORT || 3333, () => console.log(`📗: Listening on PORT => http://localhost:${server.address().port}`))
+const server = app.listen(process.env.PORT || 3333, () => {
+    console.log(`📗: Listening on PORT => http://localhost:${server.address().port}`)
+})
 
 
