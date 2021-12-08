db20f5228d3bb3afe82fc49aa2ae3c1fcc6ce57b
Mohd Salman Ansari <salman0ansariii@gmail.com>
2021-12-08 16:20:38 +0530

    🐛  fix: #11

--- Middleware/loginVerify.js
+++ Middleware/loginVerify.js
@@ -3,12 +3,12 @@ function InstanceLoginVerification(req, res, next) {
     const key = req.query["key"]?.toString()
 
     if (!key) {
-        res.status(403).send(({ error: true, message: 'no key query was present' }))
+        return res.status(403).send(({ error: true, message: 'no key query was present' }))
     }
 
     const instance = WhatsAppInstances[key];
     if (!instance.instance.conn?.phoneConnected) {
-        res.status(401).send(({ error: true, message: "phone isn't connected" }))
+        return res.status(401).send(({ error: true, message: "phone isn't connected" }))
     }
     next()
 }
