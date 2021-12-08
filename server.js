a6e22af90bb74b9c81403357f1118ca0835c8473
Mohd Salman Ansari <salman0ansariii@gmail.com>
2021-12-08 16:59:05 +0530

    🗑 remove: unused dependency

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express()
-// const fileupload = require("express-fileupload")
 const cors = require("cors");
 const morgan = require("morgan");
 const fs = require('fs')
@@ -8,7 +7,6 @@ const swaggerUI = require("swagger-ui-express");
 const docs = require('./Docs');
 
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(docs));
-// app.use(fileupload())
 app.set('view engine', 'ejs');
 app.set('views','./Views')
 app.use(express.static('./Views'));
@@ -37,7 +35,7 @@ app.get('/', (req, res) => {
 app.get('*',(req, res) => {
     res.status(404).send({
         error: true,
-        message: "404 not found"
+        message: "404 page not found"
     });
 })
 
