cd30da45d5f37fd80241aea5b130e3de419ba0f1
Mohd Salman Ansari <salman0ansariii@gmail.com>
2022-02-12 17:38:54 +0530

    feat(middleware): login and key check

--- src/api/routes/instance.route.js
+++ src/api/routes/instance.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const controller = require('../controllers/instance.controller');
+const keyVerify = require("../middlewares/keyCheck")
 
 const router = express.Router();
 router.route('/init').get(controller.init);
-router.route('/qr').get(controller.qr);
+router.route('/qr').get(keyVerify, controller.qr);
+router.route('/info').get(keyVerify, controller.info);
+router.route('/restore').get(controller.restore);
 
 module.exports = router;
