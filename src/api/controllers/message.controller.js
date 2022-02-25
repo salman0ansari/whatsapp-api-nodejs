ace062cd91c1149cbd04a3e5a08c16842726c24a
Mohd Salman Ansari <salman0ansariii@gmail.com>
2022-02-25 17:23:08 +0530

    feat(controller): add - image, audio, video, document endpoint - add multer

--- src/api/controllers/message.controller.js
+++ src/api/controllers/message.controller.js
@@ -5,3 +5,44 @@ exports.Text = async (req, res) => {
     );
     return res.status(201).json({ error: false, data: data });
 }
+
+exports.Image = async (req, res) => {
+    const data = await WhatsAppInstances[req.query.key].sendMediaFile(
+        req.query.id,
+        req.query?.caption,
+        req.file,
+        "image"
+    );
+    return res.status(201).json({ error: false, data: data });
+}
+
+exports.Video = async (req, res) => {
+    const data = await WhatsAppInstances[req.query.key].sendMediaFile(
+        req.query.id,
+        req.query?.caption,
+        req.file,
+        "video"
+    );
+    return res.status(201).json({ error: false, data: data });
+}
+
+exports.Audio = async (req, res) => {
+    const data = await WhatsAppInstances[req.query.key].sendMediaFile(
+        req.query.id,
+        req.query?.caption,
+        req.file,
+        "audio"
+    );
+    return res.status(201).json({ error: false, data: data });
+}
+
+exports.document = async (req, res) => {
+    const data = await WhatsAppInstances[req.query.key].sendMediaFile(
+        req.query.id,
+        req.query?.caption,
+        req.file,
+        "document"
+    );
+    return res.status(201).json({ error: false, data: data });
+}
+
--- src/api/routes/message.route.js
+++ src/api/routes/message.route.js
@@ -2,8 +2,16 @@ const express = require('express');
 const controller = require('../controllers/message.controller');
 const keyVerify = require("../middlewares/keyCheck")
 const loginVerify = require("../middlewares/loginCheck")
+const multer  = require('multer')
 
 const router = express.Router();
-router.route('/text').get(controller.Text);
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage, inMemory: true }).single('file')
+
+router.route('/text').post(controller.Text);
+router.route('/image').post(upload, controller.Image);
+router.route('/video').post(upload, controller.Video);
+router.route('/audio').post(upload, controller.Audio);
+router.route('/document').post(upload, controller.Document);
 
 module.exports = router;
