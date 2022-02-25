dab2df0a8816c95bcebf37f39bf336decc2a0e7b
Mohd Salman Ansari <salman0ansariii@gmail.com>
2022-02-25 21:07:36 +0530

    feat(misc): add controller and route - add onWhatsapp endpoint

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const instanceRoutes = require('./instance.route')
 const messageRoutes = require('./message.route')
+const miscRoutes = require('./misc.route')
 
 router.get('/status', (req, res) => res.send('OK'))
 
 router.use('/instance', instanceRoutes)
 router.use('/message', messageRoutes)
+router.use('/misc', miscRoutes)
 
 module.exports = router
---
+++ src/api/routes/misc.route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const controller = require('../controllers/misc.controller')
+const keyVerify = require('../middlewares/keyCheck')
+const loginVerify = require('../middlewares/loginCheck')
+
+const router = express.Router()
+
+router.route('/onwhatsapp').get(keyVerify, loginVerify, controller.onWhatsapp)
+
+module.exports = router
