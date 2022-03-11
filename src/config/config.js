35de6bcdb44a7d4a16b1b6a4ae02b2f4300715dd
Mohd Salman Ansari <salman0ansariii@gmail.com>
2022-03-11 19:43:28 +0530

    update config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,9 @@
 // Port number
 const PORT = '3333'
-
 // URL of the Mongo DB
 const MONGODB_URL = 'mongodb://127.0.0.1:27017/WhatsAppInstance'
+// Webhook URL
+const WEBHOOK_URL = 'https://webhook.site/d0122a66-18a3-432d-b63f-4772b190dd72'
 
 module.exports = {
     port: PORT,
@@ -14,4 +15,5 @@ module.exports = {
             useUnifiedTopology: true,
         },
     },
+    webhookUrl: WEBHOOK_URL,
 }
