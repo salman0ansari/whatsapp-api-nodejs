a5f74ac4594021d37b883af6aab65e35322a7886
Mohd Salman Ansari <salman0ansariii@gmail.com>
2022-07-16 18:14:46 +0530

    fix(webhook): custom webhook #182

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -27,23 +27,31 @@ class WhatsAppInstance {
     }
     key = ''
     authState
-    allowWebhook = false
+    allowWebhook = undefined
+    webhook = undefined
+
     instance = {
         key: this.key,
         chats: [],
         qr: '',
         messages: [],
         qrRetry: 0,
+        customWebhook: '',
     }
 
     axiosInstance = axios.create({
         baseURL: config.webhookUrl,
     })
 
-    constructor(key, allowWebhook = false, webhook = null) {
+    constructor(key, allowWebhook, webhook) {
         this.key = key ? key : uuidv4()
-        this.allowWebhook = config.webhookEnabled
-        if (this.allowWebhook && webhook !== null) {
+        this.instance.customWebhook = this.webhook ? this.webhook : webhook
+        this.allowWebhook = config.allowWebhook
+            ? config.allowWebhook
+            : allowWebhook
+        if (this.allowWebhook && this.instance.customWebhook !== null) {
+            this.allowWebhook = true
+            this.instance.customWebhook = webhook
             this.axiosInstance = axios.create({
                 baseURL: webhook,
             })
@@ -286,6 +294,7 @@ class WhatsAppInstance {
         return {
             instance_key: key,
             phone_connected: this.instance?.online,
+            webhookUrl: this.instance.customWebhook,
             user: this.instance?.online ? this.instance.sock?.user : {},
         }
     }
