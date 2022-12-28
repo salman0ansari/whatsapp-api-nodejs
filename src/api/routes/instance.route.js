0a777fb61b98993633b46add147adabf89a68f47
Ak4zh <agwl.akash@gmail.com>
2022-12-28 15:02:45 +0530

    ignore loginVerify when deleting instance

--- src/api/routes/instance.route.js
+++ src/api/routes/instance.route.js
@@ -10,7 +10,7 @@ router.route('/qrbase64').get(keyVerify, controller.qrbase64)
 router.route('/info').get(keyVerify, controller.info)
 router.route('/restore').get(controller.restore)
 router.route('/logout').delete(keyVerify, loginVerify, controller.logout)
-router.route('/delete').delete(keyVerify, loginVerify, controller.delete)
+router.route('/delete').delete(keyVerify, controller.delete)
 router.route('/list').get(controller.list)
 
 module.exports = router
