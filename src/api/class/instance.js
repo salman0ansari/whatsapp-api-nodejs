a8105bfcdc76b1ab321517fbe29bac1bfbe82288
rootjack404 <57262344+rootjack404@users.noreply.github.com>
2023-02-06 11:33:31 +0700

    Add ability to input filename in urlmedia type

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -395,7 +395,7 @@ class WhatsAppInstance {
         return data
     }
 
-    async sendUrlMediaFile(to, url, type, mimeType, caption = '') {
+    async sendUrlMediaFile(to, url, type, mimeType, caption = '', filename) {
         await this.verifyId(this.getWhatsAppId(to))
 
         const data = await this.instance.sock?.sendMessage(
@@ -406,6 +406,7 @@ class WhatsAppInstance {
                 },
                 caption: caption,
                 mimetype: mimeType,
+                fileName: filename ? filename : file.originalname,
             }
         )
         return data
