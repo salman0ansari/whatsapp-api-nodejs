23f58395a1fe35d7e6b73ce4a1bce4834e450e53
Mohd Salman Ansari <salman0ansariii@gmail.com>
2023-04-12 16:40:14 +0530

    feat: add express-validator to validate request body and query parameters in init, qr, and restore handlers

--- src/controller/instance.ts
+++ src/controller/instance.ts
@@ -1,52 +1,91 @@
 import { RequestHandler } from 'express';
 import { createSession, getSession, sessionExists, bottle } from '../session.js';
+import { body, query, validationResult } from 'express-validator';
 import { qrHTML } from '../misc/qrcode.js';
 
-const init: RequestHandler = async (req, res) => {
-    try {
-        const { sessionid: sessionId, webhook = "" } = req.body;
-        if (sessionExists(sessionId)) return res.send({ error: 'session already exists' }).status(400)
+const init: RequestHandler[] = [
+    body('sessionid').notEmpty(),
+    body('webhook').optional().isURL(),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-        const sock = new createSession({ sessionId, webhook });
-        sock.create()
+            const { sessionid: sessionId, webhook = "" } = req.body;
+            if (sessionExists(sessionId)) return res.send({ error: 'session already exists' }).status(400)
 
-        return res.status(200).send({
-            message: 'session created',
-            sessionId,
-        });
+            const sock = new createSession({ sessionId, webhook });
+            sock.create()
+
+            return res.status(200).send({
+                message: 'session created',
+                sessionId,
+            });
+        }
+        catch (e) {
+            console.log(e)
+            return res.status(400).send({ error: 'session already exists' })
+        }
     }
-    catch (e) {
-        console.log(e)
-        return res.status(400).send({ error: 'session already exists' })
+]
+
+const qr: RequestHandler[] = [
+    query('sessionid').notEmpty(),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const sessionId = req.query['sessionid'] as string;
+            const sock = getSession(sessionId);
+            if (!sock) return res.status(400).send({ error: 'session not found' })
+
+            const qr = await sock.getQr()
+            if (!qr) return res.status(400).send({ error: 'qr not loaded or session invalid' })
+
+            if (qr === "connected") {
+                return res.status(200).send({ message: 'connected' });
+            }
+            return res.status(200).send(qrHTML(qr, sessionId));
+        }
+        catch (e) {
+            console.log(e)
+            return res.status(400).send({ error: 'session not found' })
+        }
     }
-}
+]
 
-const qr: RequestHandler = async (req, res) => {
-    const sessionId = req.query['sessionid'] as string;
-    const sock = getSession(sessionId);
-    if (!sock) return res.status(400).send({ error: 'session not found' })
+const restore: RequestHandler[] = [
+    query('sessionid').notEmpty(),
+    query('webhook').optional().isURL(),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-    const qr = await sock.getQr()
-    if (!qr) return res.status(400).send({ error: 'qr not loaded or session invalid' })
+            const { sessionid: sessionId, webhook = "" } = req.query as any;
+            if (sessionExists(sessionId)) return res.send({ error: 'session already exists' }).status(400)
 
-    if (qr === "connected") {
-        return res.status(200).send({ message: 'connected' });
+            const sock = new createSession({ sessionId, webhook });
+            sock.create()
+
+            return res.status(200).send({
+                message: 'session created',
+                sessionId,
+            });
+        }
+        catch (e) {
+            console.log(e)
+            return res.status(400).send({ error: 'session already exists' })
+        }
     }
-    return res.status(200).send(qrHTML(qr, sessionId));
-}
-
-const restore: RequestHandler = async (req, res) => {
-    const { sessionid: sessionId, webhook = "" } = req.query as any;
-    if (sessionExists(sessionId)) return res.send({ error: 'session already exists' }).status(400)
-
-    const sock = new createSession({ sessionId, webhook });
-    sock.create()
-
-    return res.status(200).send({
-        message: 'session created',
-        sessionId,
-    });
-}
+]
 
 
 
