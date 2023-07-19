9a2bfc31da4f35a83ff3002a7d0090a349eeb222
shintackeror <shintackeror404@gmail.com>
2023-07-20 05:53:52 +0700

    update UI

---
+++ ui/assets/axios.6b9cb268.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.374e2772.js";import{a}from"./axios.7331a07d.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
