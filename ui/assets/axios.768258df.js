6559f1ece2626afd611b0ab14c573bc44d75531f
shintackeror <shintackeror404@gmail.com>
2023-07-19 15:57:56 +0700

    init ui

---
+++ ui/assets/axios.768258df.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.73c93f01.js";import{a}from"./axios.7fafc2de.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
