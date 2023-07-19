b2b8646ce131c9e04e90516ccb69ad4018f53109
shintackeror <shintackeror404@gmail.com>
2023-07-19 15:57:34 +0700

    add serve static path

--- src/config/express.js
+++ src/config/express.js
@@ -9,6 +9,9 @@ const tokenCheck = require('../api/middlewares/tokenCheck')
 const { protectRoutes } = require('./config')
 
 app.use(cors())
+
+app.use('/ui', express.static(path.join(__dirname, '../../ui')))
+
 app.use(express.json())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
